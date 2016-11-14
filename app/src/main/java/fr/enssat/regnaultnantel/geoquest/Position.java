e8349c1f8a1fbad66124e55c4e6210e1a934027e
4r3 <4r3@users.noreply.github.com>
2016-11-14 15:27:23 +0100

    add position listener

---
+++ app/src/main/java/fr/enssat/regnaultnantel/geoquest/Position.java
@@ -0,0 +1,39 @@
+package fr.enssat.regnaultnantel.geoquest;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+
+public class Position {
+    public Position(MainActivity mainActivity) {
+        LocationManager locationManager = (LocationManager) mainActivity.getSystemService(Context.LOCATION_SERVICE);
+
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                System.out.println(location);
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+        // Register the listener with the Location Manager to receive location updates
+        try{
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        }catch (SecurityException e){
+            e.printStackTrace();
+        }
+    }
+}
